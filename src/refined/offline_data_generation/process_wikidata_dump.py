0ca68fb92a7761b6c5828b1c8e69447f61853a24
hosein <hosein.panahi98@gmail.com>
2025-03-30 10:53:24 -0400

    Print a line of wikidata dump file

--- src/refined/offline_data_generation/process_wikidata_dump.py
+++ src/refined/offline_data_generation/process_wikidata_dump.py
@@ -8,6 +8,7 @@ from types import SimpleNamespace
 
 
 def extract_useful_info(entity):
+    print(entity)
     qcode = entity['id']
     if 'en' in entity['labels']:
         entity_en_label = entity['labels']['en']['value']
